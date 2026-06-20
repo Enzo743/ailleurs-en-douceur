71c2881b11adc7f83e677363e9557a22e39795d0
Enzoooo_ <bortoenzo@gmail.com>
2026-06-20 13:46:20 +0200

    🎨 Migrated global and module CSS to SCSS; added Sass mixins and variables integration.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  sassOptions: {
+    loadPaths: ["./src/styles"],
+  },
 };
 
 export default nextConfig;
